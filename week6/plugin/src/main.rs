aff1d31f8713a6cf0a783133087fb8977024bc71
re1n-e <raghavendrabargali@gmail.com>
2025-10-20 22:33:02 +0530

    commit

---
+++ week6/plugin/src/main.rs
@@ -0,0 +1,81 @@
+use std::io::{self, Write};
+
+trait Plugin {
+    fn name(&self) -> &str;
+    fn run(&self, input: &str) -> String;
+}
+
+struct UppercasePlugin;
+impl Plugin for UppercasePlugin {
+    fn name(&self) -> &str {
+        "Uppercase"
+    }
+
+    fn run(&self, input: &str) -> String {
+        input.to_uppercase()
+    }
+}
+
+struct ReversePlugin;
+impl Plugin for ReversePlugin {
+    fn name(&self) -> &str {
+        "Reverseplugin"
+    }
+
+    fn run(&self, input: &str) -> String {
+        input.chars().rev().collect()
+    }
+}
+
+struct DuplicatePlugin;
+impl Plugin for DuplicatePlugin {
+    fn name(&self) -> &str {
+        "Duplicate"
+    }
+    fn run(&self, input: &str) -> String {
+        format!("{} {}", input, input)
+    }
+}
+
+fn main() {
+    println!("Plugin system Demo");
+
+    let plugins: Vec<Box<dyn Plugin>> = vec![
+        Box::new(UppercasePlugin),
+        Box::new(ReversePlugin),
+        Box::new(DuplicatePlugin),
+    ];
+
+    loop {
+        println!("Availabe plugin: ");
+        for (i, plugin) in plugins.iter().enumerate() {
+            println!("{}. {}", i + 1, plugin.name());
+        }
+        println!("{}. Exit", plugins.len() + 1);
+
+        let choice = input("Choose a plugin:");
+        let index = match choice.trim().parse::<usize>() {
+            Ok(num) if num >= 1 && num <= plugins.len() => num - 1,
+            Ok(num) if num == plugins.len() + 1 => {
+                println!("Exiting");
+                break;
+            }
+            _ => {
+                println!("Invalide choice");
+                continue;
+            }
+        };
+
+        let data = input("Enter input text: ");
+        let result = plugins[index].run(&data);
+        println!("Plugin output: {}", result);
+    }
+}
+
+fn input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+}
